9b2ada51375d7d726f32620b5b768cb73d20acfd
Lun-Shin Yuen <lun@cienza.net>
2024-01-20 18:48:06 -0800

    initial commit

---
+++ button-element.js
@@ -0,0 +1,45 @@
+class ButtonElement extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this._onClick = () => {};
+    }
+
+    connectedCallback() {
+        this.render();
+        this.addEventListener('click', this._onClick);
+    }
+
+    disconnectedCallback() {
+        this.removeEventListener('click', this._onClick);
+    }
+
+    set onClick(value) {
+        if (typeof value === 'function') {
+            this._onClick = value;
+        }
+    }
+
+    get onClick() {
+        return this._onClick;
+    }
+
+    render() {
+        const label = this.getAttribute('label') || 'Click me!';
+
+        this.shadowRoot.innerHTML = `
+            <style>
+                button {
+                    padding: 10px;
+                    background-color: #4CAF50;
+                    color: white;
+                    border: none;
+                    cursor: pointer;
+                }
+            </style>
+            <button>${label}</button>
+        `;
+    }
+}
+
+customElements.define('button-element', ButtonElement);
---
+++ tile-element.js
@@ -0,0 +1,43 @@
+class TileElement extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+            <style>
+                :host {
+                    margin: 5px;
+                    padding: 10px;
+                    background-color: #77aaff;
+                    border-radius: 5px;
+                    text-align: center;
+                    color: white;
+                    user-select: none;
+                    width: 50px;
+                    height: 50px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    font-size: 28px;
+                    position: relative; /* added this line */
+                }
+                .number {
+                    position: absolute;
+                    bottom: 12px;
+                    right: 12px;
+                    font-size: 14px;
+                }
+            </style>
+            <div>${this.getAttribute('letter')}</div>
+            <div class="number">${this.getAttribute('number')}</div>
+        `;
+    }
+}
+
+customElements.define('tile-element', TileElement);
